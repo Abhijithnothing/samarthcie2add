6e1f042ae1673e96c693a6a2352073f5f3d231a1
Abhijith <a00931668@gmail.com>
2025-12-23 12:01:28 +0530

    Add subtraction and division to test.py
    Added subtraction and division operations to the script.

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 a = int(input("Enter the first number "))
 b = int(input("Enter the second number "))
 c = a + b
+d = a - b
+q =  a/b
 m = a*b
 print("The sum is :", c)
 print("The product is :", m)
+print("The differene is :", d)
+print("The qutiount is :", q)
